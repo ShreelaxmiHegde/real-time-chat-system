80b00190c279b5c7b4bf42c49e3fd95669cf6dec
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-03-26 22:27:52 +0530

    setup websocket

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("@index.js")
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const port = 8080;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`);
+})

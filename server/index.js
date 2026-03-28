2a0dfd9aa509fd89aa4eec5244c2aa232f7b0a0e
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-03-28 20:38:16 +0530

    setup cors and basic message transaction

--- server/index.js
+++ server/index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
+require('dotenv').config();
 
 const port = 8080;
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log("User Connected: ", socket.id);
+
+    socket.emit('Onboard', `Welcome to Chatboard ${socket.id}`)
+
+    socket.on('chat message', (data) => {
+        console.log("message: ", data);
+        io.emit('chat message', data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log("User Disconnected:", socket.id)
+    })
 });
 
 server.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
-})
+});

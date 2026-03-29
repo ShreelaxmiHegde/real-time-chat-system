c614b41d0648e5d29e2f881099849681d0e4790b
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-03-29 22:18:32 +0530

    handle auth error with error event

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,4 +4,15 @@ const socket = io(import.meta.env.VITE_BACKEND_URL, {
     autoConnect: false
 });
 
+//development debug
+socket.onAny((event, ...args) => {
+    console.log("Event :", event, args);
+});
+
+socket.on("connect_error", (err) => {
+    if(err.message === "invalid username") {
+        console.log("invalid username!!!")
+    }
+})
+
 export default socket;

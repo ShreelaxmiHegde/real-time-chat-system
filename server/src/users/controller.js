a24fe8f55f2b65b337dbb8aca0bb9834123fdd0e
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-04-02 21:20:14 +0530

    add user signup

--- server/src/users/controller.js
+++ server/src/users/controller.js
@@ -2,33 +2,31 @@ const pool = require('../../db');
 const queries = require('./queries')
 
 const getUsers = (req, res) => {
-    console.log("getting users")
     pool.query(queries.getUsers, (err, result) => {
         if(err) {
-            console.log(err);
             return res.json({
                 message: "Failed to fetch users"
             });
-        }
+        } 
 
         return res.status(201).json(result.rows);
     });
 }
 
-// const signup = (req, res) => {
-//     pool.query(queries.signup, (err, result) => {
-//         if(err) {
-//             console.log(err);
-//             return res.json({
-//                 message: "Signup failed"
-//             });
-//         }
+const signup = (req, res) => {
+    const {username, email, password} = req.body;
+    pool.query(queries.signup, [username, email, password], (err, result) => {
+        if(err) {
+            return res.json({
+                message: "Signup failed"
+            });
+        }
 
-//         return res.status(201).json({
-//             message: "Signup was successful"
-//         });
-//     });
-// }
+        return res.status(201).json({
+            message: "Signup was successful"
+        });
+    });
+}
 
 module.exports = {
     getUsers,
--- server/src/users/queries.js
+++ server/src/users/queries.js
@@ -1,4 +1,4 @@
 const getUsers = "SELECT * FROM users";
-// const signup = "INSERT INTO users ()"
+const signup = "INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3);";
 
-module.exports = getUsers;
+module.exports = {getUsers, signup};

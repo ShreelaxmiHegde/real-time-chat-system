6d158a802f90f3e7dd74b0db2e2eed2a7a4a45ac
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2026-04-07 21:37:11 +0530

    add auth routes

--- server/app.js
+++ server/app.js
@@ -3,16 +3,18 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-const usersRoutes = require('./src/users/routes.js');
-const groupsRoutes = require('./src/groups/routes.js');
-const messageRoutes = require('./src/messages/routes.js');
+const authRoutes = require('./src/modules/auth/auth.routes.js');
+const usersRoutes = require('./src/modules/users/users.routes.js');
+const groupsRoutes = require('./src/modules/groups/groups.routes.js');
+const messageRoutes = require('./src/modules/messages/messages.routes.js');
 
 app.use(express.json()); //enable req.body parsing
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
+app.use("/", authRoutes);
 app.use("/users", usersRoutes);
 app.use("/groups", groupsRoutes);
 app.use("/groups/:groupId", messageRoutes);
